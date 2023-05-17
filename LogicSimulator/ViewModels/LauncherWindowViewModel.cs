450aab6a02de6f3f4cd2076c6c693e1af47d602e
Diablox777 <98866950+Diablox777@users.noreply.github.com>
2023-05-17 14:09:11 +0800

    Прописал комментарии к LWVM и VMB перед модернизацией визуальной части MainWindow

--- LogicSimulator/ViewModels/LauncherWindowViewModel.cs
+++ LogicSimulator/ViewModels/LauncherWindowViewModel.cs
@@ -6,56 +6,59 @@ using LogicSimulator.Views;
 using LogicSimulator.Models;
 
 namespace LogicSimulator.ViewModels {
-    public class LauncherWindowViewModel: ViewModelBase {
+    public class LauncherWindowViewModel: ViewModelBase
+    { // Контекст окна
         Window? me;
-        private static readonly MainWindow mw = new();
+        private static readonly MainWindow mw = new(); // Основное окно-редактор
 
-        public LauncherWindowViewModel() {
+        public LauncherWindowViewModel()
+        { // Конструктор, Создаем команды пользовательского ввода
             Create = ReactiveCommand.Create<Unit, Unit>(_ => { FuncCreate(); return new Unit(); });
             Open = ReactiveCommand.Create<Unit, Unit>(_ => { FuncOpen(); return new Unit(); });
             Exit = ReactiveCommand.Create<Unit, Unit>(_ => { FuncExit(); return new Unit(); });
         }
-        public void AddWindow(Window lw) => me = lw;
+        public void AddWindow(Window lw) => me = lw; // Метод для установки контекста окна
 
-        void FuncCreate() {
-            var newy = map.filer.CreateProject();
-            CurrentProj = newy;
-            mw.Show();
-            mw.Update();
-            me?.Close();
+        void FuncCreate() { //функция создания нового проекта
+            var newy = map.filer.CreateProject(); 
+            CurrentProj = newy; // Устанавливаем текущий проект
+            mw.Show(); // Показываем основное окно
+            mw.Update(); //обновляем окно
+            me?.Close(); //закрываем окно
         }
-        void FuncOpen() {
-            if (me == null) return;
+        void FuncOpen()
+        { 
+            if (me == null) return; // Если контекст окна не определен, завершаем обработчик
 
-            var selected = map.filer.SelectProjectFile(me);
-            if (selected == null) return;
+            var selected = map.filer.SelectProjectFile(me); // Выбираем проект для открытия
+            if (selected == null) return; // Если проект не выбран, завершаем обработчик
 
-            CurrentProj = selected;
+            CurrentProj = selected; // Устанавливаем текущий проект
             mw.Show();
             mw.Update();
-            me?.Close();
+            me?.Close(); //закрываем LauncherWindow
         }
         void FuncExit() {
-            me?.Close();
-            mw.Close();
+            me?.Close(); //закрываем LauncherWindow
+            mw.Close(); //закрываем MainWindow
         }
-
+        // Команды пользовательского ввода
         public ReactiveCommand<Unit, Unit> Create { get; }
         public ReactiveCommand<Unit, Unit> Open { get; }
         public ReactiveCommand<Unit, Unit> Exit { get; }
 
 
-        public static Project[] ProjectList { get => map.filer.GetSortedProjects(); }
+        public static Project[] ProjectList { get => map.filer.GetSortedProjects(); } // Коллекция всех проектов
 
-        public void DTapped(object? sender, Avalonia.Interactivity.RoutedEventArgs e) {
-            var src = (Control?) e.Source;
+        public void DTapped(object? sender, Avalonia.Interactivity.RoutedEventArgs e) { // Обработчик двойного щелчка по элементу списка проектов
+            var src = (Control?) e.Source;  // Получаем источник события
 
-            if (src is ContentPresenter cp && cp.Child is Border bord) src = bord;
-            if (src is Border bord2 && bord2.Child is TextBlock tb2) src = tb2;
+            if (src is ContentPresenter cp && cp.Child is Border bord) src = bord; // Если источник ContentPresenter, выбираем его Child элемент - Border
+            if (src is Border bord2 && bord2.Child is TextBlock tb2) src = tb2; // Если источник является Border, выбираем его Child элемент - TextBlock
 
-            if (src is not TextBlock tb || tb.Tag is not Project proj) return;
+            if (src is not TextBlock tb || tb.Tag is not Project proj) return; // Если источник не является TextBlock или его Tag не является проектом, завершаем обработчик
 
-            CurrentProj = proj;
+            CurrentProj = proj; // Устанавливаем текущий проект
 
             mw.Show();
             mw.Update();
@@ -66,6 +69,6 @@ namespace LogicSimulator.ViewModels {
          * Для тестирования
          */
 
-        public static MainWindow GetMW => mw;
+        public static MainWindow GetMW => mw;  // Метод для получения объекта MainWindow
     }
 }
